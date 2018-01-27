3780fc03d40923bd72ee4800fb7e1f089c2e603b
Mathias Göbel <goebel@sub.uni-goettingen.de>
2018-01-28 00:54:12 +0100

    update site

---
+++ push-to-page.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "build with jekyll"
+bundle exec jekyll build
+echo "store result in gh-pages branch"
+cp -r _site /tmp/
+git checkout gh-pages
+git pull
+rm -rf *
+cp -r /tmp/_site/* .
+git add --all
+git commit -m "update site"
+git push
+echo "done, switch to master…"
+git checkout master
